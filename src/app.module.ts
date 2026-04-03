3ee7bb727140f8e5134b1541a8559012ab02387e
igormatheusf <igorm.fonseca@hotmail.com>
2026-04-03 15:00:11 -0300

    auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { DatabaseModule } from './shared/database/database.module';
     }),
     UsersModule,
     DatabaseModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
